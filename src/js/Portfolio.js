0145729c26b142b5e7155553005ee78efb050511
Tomasz Czechlewski <tomasz.czechlewski@gmail.com>
2018-07-26 17:06:00 +0200

    Portfolio style added, architecture changed

--- src/js/Portfolio.js
+++ src/js/Portfolio.js
@@ -10,30 +10,33 @@ class Portfolio extends Component {
                         Moje prace
                     </h1>
                 </div>
-                <div className='gallery content'>
-                    <p>
-                        <button className="btn-left"><a href="#">Previous</a></button>
-                    </p>
-                    <div className='galleryImage '>
+                <div className='galleryContent'>
+                    <div className='galleryPart'>
+                        <div className='galleryImage '>
+                        </div>
+                        <div className='projShortText'>
+                            <ul>
+                                <li>
+                                    Lorem ipsum dolor sit.
+                                </li>
+                                <li>
+                                    Lorem ipsum dolor sit.
+                                </li>
+                                <li>
+                                    Lorem ipsum dolor sit.
+                                </li>
+                            </ul>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias animi architecto assumenda blanditiis commodi cupiditate delectus deserunt dignissimos distinctio dolor eum eveniet fugit hic id iure laudantium nam nisi optio placeat provident quae quam quasi reprehenderit, similique veniam veritatis!
+                        </div>
                     </div>
-                    <div className='projShortText'>
-                        <ul>
-                            <li>
-                                Lorem ipsum dolor sit.
-                            </li>
-                            <li>
-                                Lorem ipsum dolor sit.
-                            </li>
-                            <li>
-                                Lorem ipsum dolor sit.
-                            </li>
-                        </ul>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias animi architecto assumenda blanditiis commodi cupiditate delectus deserunt dignissimos distinctio dolor eum eveniet fugit hic id iure laudantium nam nisi optio placeat provident quae quam quasi reprehenderit, similique veniam veritatis!
+                    <div className='galleryButtons'>
+                        <p>
+                            <button className="btn-left"><a href="#">Previous</a></button>
+                        </p>
+                        <p>
+                            <button className="btn-right"><a href="#">Next</a></button>
+                        </p>
                     </div>
-                    <p>
-                        <button className="btn-right"><a href="#">Next</a></button>
-                    </p>
-
                 </div>
 
             </div>
